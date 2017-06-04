ffd65d752dbac49c66b65668b73f9afb52759a93
Luke Childs <lukechilds123@gmail.com>
2017-06-04 16:57:47 +0700

    Test SSL certificate uses expected default values

--- test/create-cert.js
+++ test/create-cert.js
@@ -1,6 +1,7 @@
 import test from 'ava';
 import pify from 'pify';
 import pem from 'pem';
+import differenceInDays from 'date-fns/difference_in_days';
 import createCert from '../';
 
 test('createCert is a function', t => {
@@ -18,4 +19,12 @@ test('createCert() exposes expected properties', async t => {
 	Object.keys(cert).forEach(certType => properties.forEach(prop => {
 		t.true(typeof cert[certType][prop] !== 'undefined');
 	}));
+});
+
+test('SSL certificate uses expected default values', async t => {
+	const cert = await createCert();
+	const data = await pify(pem.readCertificateInfo)(cert.keys.cert);
+
+	t.is(data.commonName, 'example.com');
+	t.is(differenceInDays(data.validity.end, data.validity.start), 365);
 });
