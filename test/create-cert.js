bf884226eb428b9c6fdc3c02cdcf7729dc86686f
Luke Childs <lukechilds123@gmail.com>
2017-06-04 16:46:54 +0700

    Test createCert() returns expected properties

--- test/create-cert.js
+++ test/create-cert.js
@@ -1,4 +1,6 @@
 import test from 'ava';
+import pify from 'pify';
+import pem from 'pem';
 import createCert from '../';
 
 test('createCert is a function', t => {
@@ -7,4 +9,13 @@ test('createCert is a function', t => {
 
 test('createCert returns a Promise', t => {
 	t.true(createCert() instanceof Promise);
+});
+
+test('createCert() exposes expected properties', async t => {
+	const cert = await createCert();
+	const properties = ['csr', 'key', 'cert', 'serviceKey'];
+
+	Object.keys(cert).forEach(certType => properties.forEach(prop => {
+		t.true(typeof cert[certType][prop] !== 'undefined');
+	}));
 });
